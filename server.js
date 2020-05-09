329094cffa7ff30ada4002c988d2d935f4fd8eb4
David <david@thejurne.com>
2020-05-09 13:21:10 -0700

    add dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+require('dotenv').config();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
