43e2a7e5732f7a01970e4f9bd11f77f79b5c7fe2
David <david@thejurne.com>
2020-05-09 13:47:29 -0700

    add routes to server

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require('mongoose');
+
+// Import models
+const books = require('./routes/books');
 
 // Configure secrets
 require('dotenv').config();
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
