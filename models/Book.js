01419a25763d9650a2b242e6e4c2bd0d5666757f
David <david@thejurne.com>
2020-05-09 13:40:28 -0700

    add book model

---
+++ models/Book.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  authors: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = Book = mongoose.model('books', BookSchema);
