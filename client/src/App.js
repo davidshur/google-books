3edf814d3e23c110aac946526626b03bc9530c8f
David <david@thejurne.com>
2020-05-20 23:02:18 -0700

    header in container

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Saved from './components/Saved';
 import Search from './components/Search';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 
 const App = () => (
   <Router>
     <Navigation />
+    <Container>
     <Header />
     <Switch>
       <Route exact path='/' component={Search} />
       <Route exact path='/saved' component={Saved} />
     </Switch>
+
+    </Container>
   </Router>
 );
 
